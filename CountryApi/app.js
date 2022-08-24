09409b29b0105aa87994b8376baaff9de0c89cde
ajsagai <ajsagai@yahoo.com>
2022-08-25 00:02:37 +0100

    first commit

---
+++ CountryApi/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#btnFetch").click(function(){
+
+//Get the City name
+var cityname = $('#cityName').val();
+alert(cityname);
+//var cityname = document.getElementById('cityName');
+
+
+//getCountry();
+
+
+$.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/city?name=' + cityname,
+        headers: { 'X-Api-Key': 'qsxKuSVmqak05+CODEO6JQ==2x8SDxtdlT4Iuy2z'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+            //Show the Results from API
+            document.getElementById("cityTextName").innerHTML = "City: " + cityname.toUpperCase();
+            document.getElementById("countryName").value= JSON.stringify(result[0].country);
+            document.getElementById("population").value= JSON.stringify(result[0].population);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+
+
+    });
+
+
+
+
+
+});
